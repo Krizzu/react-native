9344be6a2ad54492d2d026c47d0bb49e33a720d7
Krzysztof Borowy <krizzu.dev@gmail.com>
2020-03-09 12:20:56 +0100

    fix: dont launch in release

---
+++ scripts/ios-start-packager.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+if [[ \"$CONFIGURATION\" == *Release* ]]; then
+  exit 0;
+fi;
+
+echo "export RCT_METRO_PORT=${RCT_METRO_PORT}" > "${SRCROOT}/../node_modules/react-native/scripts/.packager.env"
+if [ -z "${RCT_NO_LAUNCH_PACKAGER+xxx}" ] ; then
+  if nc -w 5 -z localhost ${RCT_METRO_PORT} ; then
+    if ! curl -s "http://localhost:${RCT_METRO_PORT}/status" | grep -q "packager-status:running" ; then
+      echo "Port ${RCT_METRO_PORT} already in use, packager is either not running or not running correctly"
+      exit 2
+    fi
+  else
+    open "$SRCROOT/../node_modules/react-native/scripts/launchPackager.command" || echo "Can't start packager automatically"
+  fi
+fi
